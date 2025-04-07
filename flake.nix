0c3c0fe88f5529c71b63126ea7a130290976f021
John Childs <51126500+dajmeister@users.noreply.github.com>
2025-04-06 21:23:03 -0400

    Create basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        nixos-wsl.nixosModules.wsl
+      ];
+    };
+  };
+}
